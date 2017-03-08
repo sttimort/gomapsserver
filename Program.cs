67c50360bc5206f772cbcc39dffeaaf4f507d45c
sttimort <mihail.grb@gmail.com>
2017-03-09 00:38:39 +0300

    Добвлена реализация простого сервера он основе KestrelDemo
    На любой запрос отправляется простая html страничка, содержащая фразу
    "GoMaps API Cloud"

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace GoMapsCloudAPI
+{
+    public class Program
+    {
+        public static int Main()
+        {
+            Console.WriteLine("Running GoMaps Cloud API.");
+
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .UseUrls("http://localhost:5000");
+
+            var host = builder.Build();
+            host.Run();
+
+            return 0;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace GoMapsCloudAPI
+{
+    class Startup
+    {
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            var serverAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
+
+            app.Run(async (context) =>
+            {
+                context.Response.ContentType = "text/html";
+                
+                await context.Response.WriteAsync("<p>GoMaps API Cloud</p>");
+            });
+        }
+    }
+}
