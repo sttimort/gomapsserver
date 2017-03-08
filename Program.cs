9935f8d7cf8642ec27d2336802566f2f3812493e
sttimort <mihail.grb@gmail.com>
2017-03-09 01:47:30 +0300

    Server now listens on addresses passed through cmd (default otherwise)

--- Program.cs
+++ Program.cs
@@ -7,14 +7,18 @@ namespace GoMapsCloudAPI
 {
     public class Program
     {
-        public static int Main()
+        public static int Main(string[] args)
         {
             Console.WriteLine("Running GoMaps Cloud API.");
 
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var builder = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseStartup<Startup>()
-                .UseKestrel()
-                .UseUrls("http://localhost:5000");
+                .UseKestrel();
 
             var host = builder.Build();
             host.Run();
